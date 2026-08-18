119a68944165784b87449276bc769d1af8709a2c
Anik Pal <100011710+Shaneomax@users.noreply.github.com>
2026-08-18 12:44:53 +0600

    Wave system added

--- Assets/V0/Scripts/ManagerClass/EnemyManager.cs
+++ Assets/V0/Scripts/ManagerClass/EnemyManager.cs
@@ -1,17 +1,33 @@
+using System.Collections.Generic;
 using UnityEngine;
+
+[System.Serializable]
+public class WaveDetails
+{
+    public int BasicEnemy;
+    public float FastEnemy;
+}
 
 public class EnemyManager : MonoBehaviour
 {
+    [SerializeField] private WaveDetails _currentWave;
+    [SerializeField] private List<GameObject> _enemiesToCreate;
     [SerializeField] private float _spawnCooldown;
     private float _spawnTimer;
-    [SerializeField] private GameObject _enemies;
+    [SerializeField] private GameObject _basicEnemies;
+    [SerializeField] private GameObject _fastEnemies;
     [SerializeField] private Transform _respawn;
+
+    private void Start()
+    {
+        _enemiesToCreate = NewEnemyWave();
+    }
 
     private void Update()
     {
         _spawnTimer -= Time.deltaTime;
 
-        if(_spawnTimer <= 0)
+        if(_spawnTimer <= 0 && _enemiesToCreate.Count > 0)
         {
             CreateEnemy();
             _spawnTimer = _spawnCooldown;
@@ -20,6 +36,33 @@ public class EnemyManager : MonoBehaviour
 
     private void CreateEnemy()
     {
-        GameObject newEnemy = Instantiate(_enemies, _respawn.position, _respawn.rotation);
+        GameObject randomEnemy = GetRandomEnemy();
+        GameObject newEnemy = Instantiate(randomEnemy, _respawn.position, _respawn.rotation);
+    }
+
+    private GameObject GetRandomEnemy()
+    {
+        int randomIndex = Random.Range(0, _enemiesToCreate.Count);
+        GameObject randomEnemy = _enemiesToCreate[randomIndex];
+        _enemiesToCreate.RemoveAt(randomIndex);
+
+        return randomEnemy;
+    }
+
+    private List<GameObject> NewEnemyWave()
+    {
+        List<GameObject> newEnemyList = new List<GameObject>();
+
+        for(int i = 0; i < _currentWave.BasicEnemy; i++)
+        {
+            newEnemyList.Add(_basicEnemies);
+        }
+
+        for(int i = 0; i < _currentWave.FastEnemy; i++)
+        {
+            newEnemyList.Add(_fastEnemies);
+        }
+
+        return newEnemyList;
     }
 }
